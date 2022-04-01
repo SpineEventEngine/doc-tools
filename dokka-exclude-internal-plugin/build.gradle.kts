3f475e6634dc17e6a45c0f675408f19fa92196b5
tripolkaandrey <andrii.trypilka@nure.ua>
2022-04-01 14:25:50 +0300

    Use Dokka dependency object

--- dokka-exclude-internal-plugin/build.gradle.kts
+++ dokka-exclude-internal-plugin/build.gradle.kts
@@ -1,3 +1,5 @@
+import io.spine.internal.dependency.Dokka
+
 plugins {
     `maven-publish`
 }
@@ -6,12 +8,10 @@ repositories {
     mavenCentral()
 }
 
-val dokkaVersion = "1.6.10"
-
 dependencies {
     val spine = io.spine.internal.dependency.Spine(project)
     implementation(spine.base)
 
-    compileOnly("org.jetbrains.dokka:dokka-core:$dokkaVersion")
-    implementation("org.jetbrains.dokka:dokka-base:$dokkaVersion")
+    compileOnly("org.jetbrains.dokka:dokka-core:${Dokka.version}")
+    implementation("org.jetbrains.dokka:dokka-base:${Dokka.version}")
 }
