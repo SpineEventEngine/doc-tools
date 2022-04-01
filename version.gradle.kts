961ba5163004c9fec1924e9043154ca5bb4c9354
tripolkaandrey <andrii.trypilka@nure.ua>
2022-04-01 14:57:11 +0300

    Increase version to publish

--- version.gradle.kts
+++ version.gradle.kts
@@ -26,4 +26,4 @@
 
 val spineBaseVersion by extra("2.0.0-SNAPSHOT.74")
 val toolBaseVersion by extra("2.0.0-SNAPSHOT.74")
-val versionToPublish by extra("2.0.0-SNAPSHOT.75")
+val versionToPublish by extra("2.0.0-SNAPSHOT.76")
